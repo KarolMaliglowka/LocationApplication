9f684f0b2303bd33f27b5ed4ee3c170931dd3620
Karol Maligowka <karol_maliglowka@o2.pl>
2020-08-09 13:44:55 +0200

    test

--- LA.Web/Startup.cs
+++ LA.Web/Startup.cs
@@ -48,7 +48,7 @@ namespace LA.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "~/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
