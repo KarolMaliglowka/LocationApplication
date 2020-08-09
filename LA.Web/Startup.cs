5690c1ef18124ac6e5fc0d325c02f9a455588961
Karol Maligowka <karol_maliglowka@o2.pl>
2020-08-09 16:03:12 +0200

    test

--- LA.Web/Startup.cs
+++ LA.Web/Startup.cs
@@ -38,44 +38,16 @@ namespace LA.Web
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();
-
+            app.UseStaticFiles("/locationapplication");
+            app.UsePathBase("/locationapplication");
             app.UseRouting();
-            app.UseStaticFiles();
             app.UseAuthorization();
-            app.UsePathBase("/locationapplication/");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.Use(async (context, next) =>
-            {
-                // TODO Check if host is valid based on hosts in ForwardedHeadersOptions
-                if (context.Request.Headers.TryGetValue("X-Path-Base", out var pathBase) &&
-                    context.Request.Path.StartsWithSegments(pathBase.Last(), out var matchedPath, out var remainingPath))
-                {
-                    try
-                    {
-                        var originalPath = context.Request.Path;
-                        var originalPathBase = context.Request.PathBase;
-                        context.Request.PathBase = matchedPath;
-                        context.Request.Path = remainingPath;
-                        await next();
-                    }
-                    finally
-                    {
-                        context.Request.PathBase = new PathString(matchedPath);
-                        context.Request.Path = new PathString(remainingPath);
-                    }
-                }
-                else
-                {
-                    await next();
-                }
             });
         }
     }
