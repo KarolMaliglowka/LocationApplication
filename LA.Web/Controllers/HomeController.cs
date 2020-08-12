34e5c3013c98c2c631b22303a6cefff93371edce
Karol <38730736+KarolMaliglowka@users.noreply.github.com>
2020-08-12 12:44:20 +0200

    Update HomeController.cs

--- LA.Web/Controllers/HomeController.cs
+++ LA.Web/Controllers/HomeController.cs
@@ -9,7 +9,6 @@ using LA.Web.Models;
 
 namespace LA.Web.Controllers
 {
-    [Route("locationapplication/[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
