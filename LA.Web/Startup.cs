9d3c99a1043b5853d15b3d7bd6e258abd074ba02
Karol <38730736+KarolMaliglowka@users.noreply.github.com>
2020-08-12 11:53:28 +0200

    Update Startup.cs

--- LA.Web/Startup.cs
+++ LA.Web/Startup.cs
@@ -48,18 +48,22 @@ namespace LA.Web
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
+          //  app.UseEndpoints(endpoints =>
+     //       {
+     //           endpoints.MapControllerRoute(
+     //               name: "default",
+     //               pattern: "{controller=Home}/{action=Index}/{id?}");
+     //       });
 
             var rewriteOptions = new RewriteOptions()
                          .AddRedirect("/", "/locationapplication");
 
             app.UseRewriter(rewriteOptions);
 
+            app.UseEndpoints(endpoints =>
+            {
+                 endpoints.MapControllers();
+            });
         }
     }
 }
